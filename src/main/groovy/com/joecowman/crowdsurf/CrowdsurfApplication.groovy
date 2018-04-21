79600f815743d48b4c923051422834e8bc24dfde
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-04-20 23:07:57 -0500

    Initial Commit

---
+++ src/main/groovy/com/joecowman/crowdsurf/CrowdsurfApplication.groovy
@@ -0,0 +1,12 @@
+package com.joecowman.crowdsurf
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CrowdsurfApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run CrowdsurfApplication, args
+	}
+}
