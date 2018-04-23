29490c985eb2a8e23105c5bdc02305a3d5960a5e
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-04-23 00:35:54 -0500

    Reword keyword

--- src/main/groovy/com/joecowman/crowdsurf/game/util/StrUtil.groovy
+++ src/main/groovy/com/joecowman/crowdsurf/game/util/StrUtil.groovy
@@ -4,9 +4,9 @@ class StrUtil {
 
     static List<String> vowels = ['a', 'e', 'i', 'o', 'u']
 
-    //Adds s if value not 1
-    static String pl(int i) {
-        return (i == 1) ? '' : 's'
+    //Adds suffix if value not 1
+    static String pl(int i, String suffix = 's') {
+        return (i == 1) ? '' : suffix
     }
 
     //Returns number with positive sign if positive (zero is positive)
