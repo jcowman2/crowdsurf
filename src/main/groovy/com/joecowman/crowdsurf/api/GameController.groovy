9f1b4864da3a767ddba50c6ba7da0360853ba5b5
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-04-23 18:09:06 -0500

    Help event

--- src/main/groovy/com/joecowman/crowdsurf/api/GameController.groovy
+++ src/main/groovy/com/joecowman/crowdsurf/api/GameController.groovy
@@ -19,11 +19,20 @@ class GameController {
                 title: "Crowdsurf",
                 author: "Joe Cowman",
                 version: "1.0 (SNAPSHOT 5)",
-                website: "http://joecowman.com",
-                description: "Under development for Ludum Dare 41."
+                website: "https://github.com/jcowman2/crowdsurf",
+                description: "A text-based music performance game. Developed for Ludum Dare 41 (\"Combine Two Incompatible Genres\")."
         )
 
         return ResponseEntity.ok(info)
+    }
+
+    @GetMapping("/welcome")
+    ResponseEntity welcome() {
+        String welcome = "Welcome to Crowdsurf!\n" +
+                "If this is your first time playing, type \"help\" for a few instructions.\n" +
+                "Otherwise, type \"start\" to begin the game. Have fun!"
+
+        return ResponseEntity.ok(welcome)
     }
 
     @PostMapping("/play")
--- src/main/groovy/com/joecowman/crowdsurf/game/CommandParser.groovy
+++ src/main/groovy/com/joecowman/crowdsurf/game/CommandParser.groovy
@@ -10,44 +10,48 @@ class CommandParser {
     static GameInstance parse(String command, GameState state, Object payload) {
         GameEvent event
 
-        //Game not yet started
-        if (!state) {
-            if (command.startsWith("start") || command.startsWith("new") || command.startsWith("play")) {
-                event = new ClientActionRequiredEvent(action: Constants.POST_SONG_NAMES)
-            } else if (command == Constants.POST_SONG_NAMES) {
-                state = new GameState()
-                event = new NewGameEvent(songOptions: payload as List<String>)
-            } else {
-                event = new IllegalCommandEvent("You can't do that yet! Type \"start\" to start the game.")
-            }
-        //Current Game
+        if (command == "help") {
+            event = new HelpEvent()
         } else {
-            state.commandNumber++
-
-            if (command.isAllWhitespace()) {
-                event = new IllegalCommandEvent("Please enter a command.")
-
-            //Song Commands
-            } else if (state.currentSong) {
-
-                if (command.startsWith("*stop")) {
-                    event = new SongEndEvent("You cut the band off. They stop playing and glare at you.")
-                } else if (command.startsWith("*key")) {
-                    event = new PrintKeywordsEvent()
-                } else if (command == Constants.SONG_TIMEOUT_COMMAND) {
-                    event = new LyricTimeoutEvent()
-                } else if (command == Constants.SONG_DURATION_OVER) {
-                    event = new SongEndEvent()
+            //Game not yet started
+            if (!state) {
+                if (command.startsWith("start") || command.startsWith("new") || command.startsWith("play")) {
+                    event = new ClientActionRequiredEvent(action: Constants.POST_SONG_NAMES)
+                } else if (command == Constants.POST_SONG_NAMES) {
+                    state = new GameState()
+                    event = new NewGameEvent(songOptions: payload as List<String>)
                 } else {
-                    event = new AddLineEvent(new LyricLine(text: command))
+                    event = new IllegalCommandEvent("You can't do that yet! Type \"start\" to start the game.")
                 }
-
-            } else if (command.startsWith("next")) {
-                event = new NewSongEvent()
-            } else if (command == Constants.SONG_DURATION_OVER || command == Constants.SONG_TIMEOUT_COMMAND) {
-                event = new NoActionEvent() //Delayed song over automated event
+                //Current Game
             } else {
-                event = new IllegalCommandEvent("You can't do that now! Type \"next\" to play the next song.")
+                state.commandNumber++
+
+                if (command.isAllWhitespace()) {
+                    event = new IllegalCommandEvent("Please enter a command.")
+
+                    //Song Commands
+                } else if (state.currentSong) {
+
+                    if (command.startsWith("*stop")) {
+                        event = new SongEndEvent("You cut the band off. They stop playing and glare at you.")
+                    } else if (command.startsWith("*key")) {
+                        event = new PrintKeywordsEvent()
+                    } else if (command == Constants.SONG_TIMEOUT_COMMAND) {
+                        event = new LyricTimeoutEvent()
+                    } else if (command == Constants.SONG_DURATION_OVER) {
+                        event = new SongEndEvent()
+                    } else {
+                        event = new AddLineEvent(new LyricLine(text: command))
+                    }
+
+                } else if (command.startsWith("next")) {
+                    event = new NewSongEvent()
+                } else if (command == Constants.SONG_DURATION_OVER || command == Constants.SONG_TIMEOUT_COMMAND) {
+                    event = new NoActionEvent() //Delayed song over automated event
+                } else {
+                    event = new IllegalCommandEvent("You can't do that now! Type \"next\" to play the next song.")
+                }
             }
         }
 
