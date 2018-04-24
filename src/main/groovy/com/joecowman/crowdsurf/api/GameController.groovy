e648faf95ef39d4996095f667c2749e5e51d9cad
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-04-23 20:23:55 -0500

    1.0 Release

--- src/main/groovy/com/joecowman/crowdsurf/api/GameController.groovy
+++ src/main/groovy/com/joecowman/crowdsurf/api/GameController.groovy
@@ -18,7 +18,7 @@ class GameController {
         GameInfo info = new GameInfo(
                 title: "Crowdsurf",
                 author: "Joe Cowman",
-                version: "1.0 (SNAPSHOT 5)",
+                version: "1.0",
                 website: "https://github.com/jcowman2/crowdsurf",
                 description: "A text-based music performance game. Developed for Ludum Dare 41 (\"Combine Two Incompatible Genres\")."
         )
